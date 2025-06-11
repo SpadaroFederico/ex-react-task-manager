bad6ece8f2d36a18206739f2f0e6b5ac936b3a9a
Federico Spadaro <federicospadaro2@gmail.com>
2025-06-11 16:27:17 +0200

    milestone 5

--- TASK_MANAGER/src/pages/AddTask.jsx
+++ TASK_MANAGER/src/pages/AddTask.jsx
@@ -1,8 +1,58 @@
+import React, { useRef, useState } from "react";
+
 function AddTask() {
+
+  const [error,setError] = useState("");
+  const [title, setTitle] = React.useState("");
+  const descriptionRef = useRef("");
+  const statusRef = useRef(null);
+  const symbols = "!@#$%^&*()-_=+[]{}|;:'\",.<>?/`~";
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (title.trim() === "") {
+      setError("Il titolo del task non può essere vuoto.");
+      return;
+    }
+
+    if ( title.split("").some(char => symbols.includes(char))){
+      setError("Il titolo del task non può contenere caratteri speciali.");
+      return;
+    }
+    
+    
+
+    console.log({
+    title: title,
+    description: descriptionRef.current.value,
+    status: statusRef.current.value,
+  })};
+
   return (
     <div>
-      <h1>Aggiungi un nuovo Task</h1>
-      {/* Qui ci sarà il form per aggiungere i task */}
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Titolo del Task" 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        
+        {error && <p style={{ color: "red" }}>{error}</p>}
+        
+        <textarea 
+          ref={descriptionRef} 
+          value={descriptionRef.current.value}
+          placeholder="descrizione del task"
+        />
+        <select ref={statusRef} type="text" placeholder="stato del Task">
+          <option value="To do">To do</option>
+          <option value="Doing">Doing</option>
+          <option value="Done">Done</option>
+        </select>
+        <button onClick={handleSubmit}>INVIA</button>
+      </form>
     </div>
   );
 }
